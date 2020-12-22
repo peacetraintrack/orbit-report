f7282cfd1f1cd4b3a327cf28cf997a29e770df49
Brigid C <b@c.com>
2020-12-21 22:11:05 -0600

    created Satellite class

---
+++ src/app/satellite.ts
@@ -0,0 +1,9 @@
+
+
+name: string;
+orbitType: string;
+type: string;
+operational: boolean;
+launchDate: string;
+
+constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean)
