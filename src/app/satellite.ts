3c852449b49cfca7840670e8144fb831a5378204
Brigid C <b@c.com>
2020-12-22 13:02:23 -0600

    taking a break to review Angular loops

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -1,9 +1,15 @@
+export class Satellite {
+    name: string;
+    orbitType: string;
+    type: string;
+    operational: boolean;
+    launchDate: string;
 
-
-name: string;
-orbitType: string;
-type: string;
-operational: boolean;
-launchDate: string;
-
-constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean)
+    constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
+        this.name = name;
+        this.type = type;
+        this.launchDate = launchDate;
+        this.orbitType = orbitType;
+        this.operational = operational;
+    }
+}
